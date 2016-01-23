feed0aa996f981c122f25c27aed6458c86778d3e
trumpet <zh19970205@126.com>
2016-01-23 15:04:27 +0000

    tab

--- static/c/c.js
+++ static/c/c.js
@@ -33,3 +33,24 @@ if(IsPC()){
 else {
     document.getElementById("size").innerHTML=".i1{font-size: 500%;}.o1{font-size: 500%;}.b{font-size: 500%}"
 }
+document.querySelector("textarea").addEventListener('keydown',function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var target = e.target;
+        var value = target.value;
+
+        // set textarea value to: text before caret + tab + text after caret
+        target.value = value.substring(0, start)
+                    + "\t"
+                    + value.substring(end);
+
+        // put caret at right position again (add one for the tab)
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+},false);
