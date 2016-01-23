c7dae603bd04d9ba7e8448c77f70a4f5bc1de02a
trumpet <zh19970205@126.com>
2016-01-23 09:47:49 +0000

    code

--- static/python/python.js
+++ static/python/python.js
@@ -1,6 +1,6 @@
 run=function(){
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET","/python?run="+document.getElementById("in").value, false);
+    xmlHttp.open("GET","/python?run="+encodeURIComponent(document.getElementById("in").value), false);
     xmlHttp.send(null);
     document.getElementById("out").value=xmlHttp.responseText;
     return true
