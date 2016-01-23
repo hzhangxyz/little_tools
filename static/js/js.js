4fcbb2587e26fdd2082fc80d9b410d933696dbfc
trumpet <zh19970205@126.com>
2016-01-23 08:02:00 +0000

    indent

--- static/js/js.js
+++ static/js/js.js
@@ -1,76 +1,76 @@
-			IsPC=function(){   
-				var system = {        
-					win :false,       
-					mac :false,       
-					xll :false
-				}
-				var p = navigator.platform
-				system.win = p.indexOf("Win") == 0
-				system.mac = p.indexOf("Mac") == 0
-				system.x11 = (p =="X11") || (p.indexOf("Linux") == 0)
-				if(system.win || system.mac || system.xll)
-					return true
-				else
-					return false
-			}
-			pre=function(n,p){
-				var t=-1
-			        k=p.keyCode
-				if(k==13){
-				        if(p.shiftKey){
-					        if(cal(n)){
-						        if(n==s)mak(++s)
-    						        t=s
-						}
-					}
-				}
-				else if(k==38)
-					if(n==0)
-						t=0
-					else
-						t=n-1
-				else if(k==40)
-					if(n==s)
-						t=s
-					else
-						t=n+1
-				if(t!=-1){
-					var e=document.getElementById("i"+t)
-					e.select()
-					return false
-				}
-				return true;
-			}
-			mak=function(n){
-				var p=document.createElement("div")
-				p.align="center"
-				p.innerHTML='\
-					<p class=i1 onclick="getElementById(\'i\'+'+n+').focus()">\
-						<span class=i3 align="center">></span>\
-						<input class=i2 id=i'+n+'\
-							onkeydown="return pre('+n+',event)"\
-							onfocus="this.select()"\
-							onMouseOver="this.style.borderColor=\'#55CBDA\'"\
-							onMouseOut="this.style.borderColor=\'white\'">\
-						</input>\
-					</p>\
-					<p class=o1 onclick="getElementById(\'o\'+'+n+').focus()">\
-						<span class=o3 align="center">=</span>\
-						<input class=o2 id=o'+n+' readonly="readonly"\
-							onfocus="this.select()"\
-							onMouseOver="this.style.borderColor=\'#4BE28A\'"\
-							onMouseOut="this.style.borderColor=\'white\'">\
-						</input>\
-					</p>'
-				document.body.appendChild(p)
-			}
-			cal=function(n){
-				var i=document.getElementById("i"+n)
-				var o=document.getElementById("o"+n)
-				if(i.value!=""){
-					o.value=eval(i.value,scope)
-					return true
-				}
-				else
-					return false
-			}
+IsPC=function(){   
+    var system = {        
+	win :false,       
+	mac :false,       
+	xll :false
+    }
+    var p = navigator.platform
+    system.win = p.indexOf("Win") == 0
+    system.mac = p.indexOf("Mac") == 0
+    system.x11 = (p =="X11") || (p.indexOf("Linux") == 0)
+    if(system.win || system.mac || system.xll)
+	return true
+    else
+	return false
+}
+pre=function(n,p){
+    var t=-1
+    k=p.keyCode
+    if(k==13){
+	if(p.shiftKey){
+	    if(cal(n)){
+		if(n==s)mak(++s)
+    		t=s
+	    }
+	}
+    }
+    else if(k==38)
+	if(n==0)
+	    t=0
+    else
+	t=n-1
+    else if(k==40)
+	if(n==s)
+	    t=s
+    else
+	t=n+1
+    if(t!=-1){
+	var e=document.getElementById("i"+t)
+	e.select()
+	return false
+    }
+    return true;
+}
+mak=function(n){
+    var p=document.createElement("div")
+    p.align="center"
+    p.innerHTML='\
+<p class=i1 onclick="getElementById(\'i\'+'+n+').focus()">\
+<span class=i3 align="center">></span>\
+<input class=i2 id=i'+n+'\
+onkeydown="return pre('+n+',event)"\
+onfocus="this.select()"\
+onMouseOver="this.style.borderColor=\'#55CBDA\'"\
+onMouseOut="this.style.borderColor=\'white\'">\
+</input>\
+</p>\
+<p class=o1 onclick="getElementById(\'o\'+'+n+').focus()">\
+<span class=o3 align="center">=</span>\
+<input class=o2 id=o'+n+' readonly="readonly"\
+onfocus="this.select()"\
+onMouseOver="this.style.borderColor=\'#4BE28A\'"\
+onMouseOut="this.style.borderColor=\'white\'">\
+</input>\
+</p>'
+    document.body.appendChild(p)
+}
+cal=function(n){
+    var i=document.getElementById("i"+n)
+    var o=document.getElementById("o"+n)
+    if(i.value!=""){
+	o.value=eval(i.value,scope)
+	return true
+    }
+    else
+	return false
+}
