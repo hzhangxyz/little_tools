c08ba77e9b3cc2e4c4c0dd27ecb472b23a3c9b48
trumpet <zh19970205@126.com>
2016-01-23 12:39:48 +0000

    some bug...

--- dynamics/c/views.py
+++ dynamics/c/views.py
@@ -6,14 +6,14 @@ import sys, os
 def c():
         if "run" in request.args.keys():
                 src=request.args["run"]
-		name=str(hash(src))
+		name="n%s"%str(hash(src))
 		inp=open('/tmp/tOoLs/%s.c'%name,'w')
                 inp.write(src)
                 inp.close()
+                os.system("rm /tmp/tOoLs/%s.out"%name)
                 os.system("touch /tmp/tOoLs/%s.out"%name)
                 try:
                         os.system("gcc /tmp/tOoLs/%s.c -o /tmp/tOoLs/%s 1>>/tmp/tOoLs/%s.out 2>>/tmp/tOoLs/%s.out"%(name,name,name,name))
-                        os.system("echo \"running...\" >>/tmp/tOoLs/%s.out"%name)
                         os.system("/tmp/tOoLs/%s 1>>/tmp/tOoLs/%s.out 2>>/tmp/tOoLs/%s.out"%(name,name,name))
                 except:
                         pass
