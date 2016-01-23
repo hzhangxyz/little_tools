d2973345ee6bfcf710329814a8ecd3851cc978b4
trumpet <zh19970205@126.com>
2016-01-23 09:14:50 +0000

    python ui first step

--- static/python/python.js
+++ static/python/python.js
@@ -1,3 +1,7 @@
 run=function(){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET","/python?run="+document.getElementById("in").value, false);
+    xmlHttp.send(null);
+    document.getElementById("out").value=xmlHttp.responseText;
     return true
 }
