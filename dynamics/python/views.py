4335a0bf52a26ceee158789c8e50c78af3413ecc
trumpet <zh19970205@126.com>
2016-01-23 08:54:55 +0000

    python ui

--- dynamics/python/views.py
+++ dynamics/python/views.py
@@ -1,20 +1,24 @@
-from flask import request
+from flask import request, render_template
 from tools import tools
 import sys, StringIO
 
 @tools.route("/python")
 def python():
-	if "run" in request.args.keys():
-		src=request.args["run"]
-		tmp=sys.stdout
-		sys.stdout=StringIO.StringIO()
-		try:
-			exec(src)
-		except Exception,e:
-			return e.__repr__()
-		ans=sys.stdout.getvalue()
-		sys.stdout.close()
-		sys.stdout=tmp
-		return ans
-	else:
-		return ""	
+        if "run" in request.args.keys():
+                src=request.args["run"]
+                tmp=sys.stdout
+                sys.stdout=StringIO.StringIO()
+                try:
+                        exec(src)
+                except Exception,e:
+                        return e.__repr__()
+                ans=sys.stdout.getvalue()
+                sys.stdout.close()
+                sys.stdout=tmp
+                return ans
+        else:
+                file=open("static/python/python.html","r")
+                data=file.read()
+                file.close()
+                return data
+
---
+++ static/python/python.js
@@ -0,0 +1,3 @@
+run=function(){
+    return true
+}
