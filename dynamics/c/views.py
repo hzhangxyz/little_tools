d764221b827cb24881bb8dbb63f27ef9fe14a7bf
trumpet <zh19970205@126.com>
2016-01-25 18:25:29 +0800

    null input gcc

--- dynamics/c/views.py
+++ dynamics/c/views.py
@@ -13,7 +13,7 @@ def c():
                 os.system("rm /tmp/tOoLs/%s.out"%name)
                 os.system("touch /tmp/tOoLs/%s.out"%name)
                 try:
-                        os.system("gcc /tmp/tOoLs/%s.c -o /tmp/tOoLs/%s 1>>/tmp/tOoLs/%s.out 2>>/tmp/tOoLs/%s.out"%(name,name,name,name))
+                        os.system("gcc /tmp/tOoLs/%s.c -o /tmp/tOoLs/%s </dev/null 1>>/tmp/tOoLs/%s.out 2>>/tmp/tOoLs/%s.out"%(name,name,name,name))
                         os.system("/tmp/tOoLs/%s </dev/null 1>>/tmp/tOoLs/%s.out 2>>/tmp/tOoLs/%s.out"%(name,name,name))
                 except Exception,e:
                         return e.__repr__()
