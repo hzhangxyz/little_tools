b51437dc6e757a1f646d7d5c2b2f149a270e832a
trumpet <zh19970205@126.com>
2016-01-27 01:11:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ wget https://raw.githubusercontent.com/zh19970205/little_tools/master/Dockerfile
 docker build -t tools .
 docker run -d -p 80:80 -m 512m --name=tools --cpu-period=50000 --cpu-quota=25000 tools
 while((1));do
+	sleep 1
 	OK=NOT
 	OK=$(curl -m 1 'http://localhost/python?run=print(%22OK%22)')
 	if [ $OK != OK];then
