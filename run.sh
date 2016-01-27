89b1188e88f1fd25614879456bfb62801d6fd43d
trumpet <zh19970205@126.com>
2016-01-27 10:09:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ while((1));do
 	sleep 1
 	OK=NOT
 	OK=$(curl -m 1 'http://localhost/python?run=print(%22OK%22)' 2>/dev/null)
-	if [ $OK != OK];then
+	if [ $OK != OK ] ; then
 		docker kill tools
 		docker rm tools
 		docker run -d -p 80:80 -m 512m --name=tools --cpu-period=50000 --cpu-quota=25000 tools
