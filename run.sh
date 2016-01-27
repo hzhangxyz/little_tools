be57bd3a280c8f50cf5968c701678998c69e287f
trumpet <zh19970205@126.com>
2016-01-27 19:17:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ while((1));do
 	OK=`curl -m 1 'http://localhost/python?run=print(%22OK%22)' 2>/dev/null`OK
 	if [ $OK != OKOK ] ; then
 		docker kill $NAME &
-		docker rm $NAME &
 		NAME=`docker run -d -p 80:80 -m 512m --cpu-period=50000 --cpu-quota=25000 tools`
 	fi
 done;
